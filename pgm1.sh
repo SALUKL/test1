9d3bc00bdf1cbaede0bd82d517800883ac80f3f6
SALUKL <shalukl312@gmail.com>
2019-04-05 15:37:46 +0530

    initial commit

---
+++ pgm1.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+echo "The current directory is $PWD"
+
+echo "The logged user is $USER"
+
+echo "The current shell is $SHELL"
+
+echo "The home directory is $HOME"
+
+echo "The system architecture is $(arch)"
+
+echo "The current path is $PATH"
